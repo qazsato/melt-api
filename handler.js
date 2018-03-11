b411941b7a5c70338c7b854c5287712aa4431a57
qazsato <qazsato@gmail.com>
2018-03-11 22:47:37 +0900

    サイトのHTMLを全て返却するように修正

--- handler.js
+++ handler.js
@@ -1,16 +1,14 @@
 'use strict';
 
+const axios = require('axios');
+
 module.exports.getWebTitle = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const url = event.queryStringParameters.url;
+  axios.get(url).then((e) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(e),
+    };
+    callback(null, response);
+  });
 };
