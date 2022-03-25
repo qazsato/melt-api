386c6ed18a243e17d9f7ef02dc159bad0137b911
qazsato <qazsato@gmail.com>
2022-03-25 19:25:17 +0900

    POST /images

--- functions/images/main.go
+++ functions/images/main.go
@@ -2,14 +2,96 @@ package main
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"regexp"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+type S3PutObjectAPI interface {
+	PutObject(ctx context.Context,
+		params *s3.PutObjectInput,
+		optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+}
+
+func PutFile(c context.Context, api S3PutObjectAPI, input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+	return api.PutObject(c, input)
+}
+
+func GetMd5(str string) string {
+	hash := md5.New()
+	defer hash.Reset()
+	hash.Write([]byte(str))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+type Image struct {
+	Key        string `json:"key"`
+	Type       string `json:"type"`
+	Attachment string `json:"attachment"`
+}
+
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	bucket := "melt-storage" // TODO: configから参照するようにする
+
+	var image Image
+	if err := json.Unmarshal([]byte(req.Body), &image); err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Request Body parse error",
+			StatusCode: 400,
+		}, nil
+	}
+
+	// 先頭の ~;base64, まではファイルデータとして不要なので空文字で置換する
+	r := regexp.MustCompile("^data:\\w+\\/\\w+;base64,")
+	fileData := r.ReplaceAllString(image.Attachment, "")
+
+	dec, err := base64.StdEncoding.DecodeString(fileData)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Decode error",
+			StatusCode: 400,
+		}, nil
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Configuration error",
+			StatusCode: 500,
+		}, nil
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	extension := strings.Split(image.Key, ".")[1]
+	key := "images/" + GetMd5(image.Key) + "." + extension
+
+	ioReaderData := strings.NewReader(string(dec))
+	input := &s3.PutObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+		Body:   ioReaderData,
+	}
+
+	_, err = PutFile(context.TODO(), client, input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Got error uploading file",
+			StatusCode: 400,
+		}, nil
+	}
+
+	url := "https://s3-ap-northeast-1.amazonaws.com/" + bucket + "/" + key
 	return events.APIGatewayProxyResponse{
-		Body:       "{\"message\": \"POST /images!\"}",
+		Body:       "{\"name\": \"" + image.Key + "\", \"url\": \"" + url + "\"}",
 		StatusCode: 200,
 	}, nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,24 @@ module github.com/qazsato/melt-api
 
 go 1.18
 
+require github.com/aws/aws-lambda-go v1.28.0
+
 require (
-	github.com/aws/aws-lambda-go v1.28.0
+	github.com/aws/aws-sdk-go-v2 v1.16.1 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.15.2 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.11.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.2 // indirect
+	github.com/aws/smithy-go v1.11.2 // indirect
 	github.com/icarus-sullivan/mock-lambda v0.0.0-20220115083805-e065469e964a // indirect
 )
